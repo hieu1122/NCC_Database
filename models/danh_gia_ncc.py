5a30de822d5e4045ab73137721d598e13a29c34f
hieu1122 <nthieu4402@gmail.com>
2024-10-07 14:07:40 +0700

    update code

--- models/danh_gia_ncc.py
+++ models/danh_gia_ncc.py
@@ -20,11 +20,11 @@ class DanhGiaNCC(models.Model):
         ('confirmed', 'Đã xác nhận'),
         ('rejected', 'Từ chối'),
         ('cancelled', 'Huỷ')
-    ], string='Trạng thái',default='draft')
+    ], string='Trạng thái', default='draft')
     user_id = fields.Many2one('res.users', string='User')
 
     ct_danh_gia_chi_tiet_ids = fields.One2many(
-        'ct.danh.gia.ncc', 'ct_danh_gia_ncc',string='')
+        'ct.danh.gia.ncc', 'ct_danh_gia_ncc', string='')
 
     tong_diem_cuoi_cung = fields.Float(
         string='Tổng điểm cuối cùng',
@@ -39,13 +39,33 @@ class DanhGiaNCC(models.Model):
         ('5', '5 sao'),
     ], string='Kết quả đánh giá', related='ct_danh_gia_chi_tiet_ids.kq_danh_gia')
 
-
     @api.model
     def create(self, vals):
         if 'ma_phieu' not in vals:
-            vals['ma_phieu'] = self.env['ir.sequence'].next_by_code('dg_ncc') or 'DGNCC0001'
+            existing_numbers = [
+                int(record.ma_phieu[8:]) for record in self.search([])
+                if record.ma_phieu.startswith('DGNCC')
+            ]
+            next_number = 1
+            while next_number in existing_numbers:
+                next_number += 1
 
-        return super(DanhGiaNCC, self).create(vals)
+            vals['ma_phieu'] = f'DGNCC{next_number:04}'
+
+        record = super(DanhGiaNCC, self).create(vals)
+
+        tieu_chi_danh_gia = self.env['tieu.chi.dg'].search([])
+
+        for tieu_chi in tieu_chi_danh_gia:
+            self.env['ct.danh.gia.ncc'].create({
+                'ct_danh_gia_ncc': record.id,
+                'tieu_chi_dg': tieu_chi.id,
+                'da_duoc_dg': False,
+                'diem_dg': None,
+            })
+
+        return record
+
 
     def action_confirm(self):
         self.trang_thai = 'confirmed'
